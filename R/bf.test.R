1dd0698a92a5ec66df090e324863cf4260711209
rgriff23 <rgriff23@gmail.com>
2015-09-16 12:54:15 -0400

    Fixed bugs in bf and lr test

--- R/bf.test.R
+++ R/bf.test.R
@@ -1,12 +1,6 @@
 bf.test = function (model1, model2) {
-	if ("Harmonic.Mean" %in% names(model1)) {
-		Hm1 = tail(model1$Results$Harmonic.Mean, 1)
-		Hm2 = tail(model2$Results$Harmonic.Mean, 1)
-	}
-	if ("HMean"%in% names(model1$Results)) {
-		Hm1 = tail(model1$Results$HMean, 1)
-		Hm2 = tail(model2$Results$HMean, 1)
-	}
+	Hm1 = tail(model1$Harmonic.Mean, 1)
+	Hm2 = tail(model2$Harmonic.Mean, 1)
 	bf = 2*(Hm1-Hm2)
 	if (Hm1 < Hm2) {BetterModel = "Model 2"; bf = -bf} else {BetterModel = "Model 1"}
 	return(data.frame(BayesFactor = bf, BetterModel = BetterModel))
--- R/lr.test.R
+++ R/lr.test.R
@@ -1,15 +1,18 @@
 lr.test = function (model1, model2) {
-	Lh1 = model1$Lh
-	Lh2 = model2$Lh
-	if (length(Lh1) != length(Lh2)) {stop("Objects must contain the same number of models.")}
+	if (nrow(model1) != nrow(model2)) {stop("Objects must contain the same number of models.")}
+	Lhs = c(mean(model1$Lh), mean(model2$Lh))
+	ind = sort(Lhs, index.return=TRUE)$ix
+	max = which.max(c(model1$Lh, model2$Lh))
+	Lh1 = Lhs[ind[1]]
+	Lh2 = Lhs[ind[2]]
 	LRstat = c()
 	pval = c()
 	for (n in 1:length(Lh1)) {
 		lrs = 2*(Lh1[n] - Lh2[n])
 		if (Lh1[n] < Lh2[n]) {lrs = -lrs}
 		pv = pchisq(lrs, df=1, lower.tail=F) 
-		LRstatistic = c(LRstatistic, lrs)
+		LRstat = c(LRstat, lrs)
 		pval = c(pval, pv)
 	}
-return(data.frame(Lh1, Lh2, LRstat, pval))
+return(data.frame(model1$Lh, model2$Lh, LRstat, pval))
 }
