931fdc9dd61b110ade771abaf602bdb15e38030e
rgriff23 <rgriff23@gmail.com>
2016-05-15 13:42:32 -0400

    Add vr feature

--- R/Continuous.R
+++ R/Continuous.R
@@ -1,4 +1,4 @@
-Continuous = function (tree, data, mode = "ML", regression = FALSE, directional = FALSE, lambda = 1, kappa = 1, delta = 1, ou = 0, tc = FALSE, mlt = 10, it = 100000, bi = 5000, sa = 100, silent=TRUE) {
+Continuous = function (tree, data, mode = "ML", regression = FALSE, directional = FALSE, vr = FALSE, lambda = 1, kappa = 1, delta = 1, ou = 0, tc = FALSE, mrca = NULL, mlt = 10, it = 100000, bi = 5000, sa = 100, lf = NULL, silent=TRUE) {
 
 # CHECK FOR PROBLEMS IN THE DATA 
 tree$node.label = NULL
@@ -20,11 +20,14 @@ if (lambda == "ML") {L = ""} else {L = lambda}
 if (kappa == "ML") {K = ""} else {K = kappa}
 if (delta == "ML") {D = ""} else {D = delta}
 if (ou == "ML") {O = ""} else {O = ou}
+if (vr == TRUE) {model = 7; mode = 2}
 input = c(model, mode)
 input = c(input, paste("lambda", L))
 input = c(input, paste("kappa", K))
 input = c(input, paste("delta", D))
 input = c(input, paste("ou", O))	
+if (vr == TRUE) {input = c(input, "VarRates")}
+if (!is.null(mrca) & vr != TRUE) {for (i in 1:length(mrca)) {input = c(input, paste("mrca", paste("mrcaNode", i, sep=""), mrca[i]))}}
 if (ncol(data) == 3 & tc == TRUE) {input = c(input, "tc")}
 if (mode == 1) {input = c(input, paste("mlt", as.numeric(mlt)))}
 if (mode == 2) {
@@ -32,7 +35,7 @@ if (mode == 2) {
 	input = c(input, paste("bi", format(bi, scientific=F)))
 	input = c(input, paste("sa", format(sa, scientific=F)))
 }
-input = c(input, paste("lf ./BTout.log.txt"))
+if (is.null(lf)) {input = c(input, paste("lf ./BTout.log.txt"))} else {input = c(input, paste("lf", lf))}
 input = c(input, "run")	
 write(input, file="./inputfile.txt") 
 ape::write.nexus(tree, file="./tree.nex", translate=T)	
@@ -42,15 +45,23 @@ write.table(data, file="./data.txt", quote=F, col.names=F, row.names=F)
 system(paste(.BayesTraitsPath, "./tree.nex", "./data.txt", "< ./inputfile.txt"), ignore.stdout = silent)
 
 # GET OUTPUT 
-FullOut = scan(file = "./BTout.log.txt", what="c", quiet=T, sep="\n")
-Results = read.table("./BTout.log.txt", skip = (grep("Tree No", FullOut) - 1), sep = "\t", header = TRUE)
+if (is.null(lf)) {lf = "./BTout.log.txt"}
+FullOut = scan(file = lf, what="c", quiet=T, sep="\n")
+Results = read.table(lf, skip = (grep("Tree No", FullOut) - 1), sep = "\t", header = TRUE)
 Results = Results[,-ncol(Results)]	
+if (vr == TRUE) {
+	skip = grep("=", scan(file = "./data.txt.PP.trees", what="c", quiet=T, sep="\n"))[1] - 1
+	Trees = read.tree(file="~/Desktop/GitHub/Evaluating_mvBM/R/data.txt.PP.trees", skip=skip, keep.multi=TRUE)
+	Results = list(Results, Trees)
+}
 
 # DELETE FILES FROM DISK
-system(paste("rm ./BTout.log.txt"))
+system(paste("rm", lf))
 system(paste("rm ./inputfile.txt"))
 system(paste("rm", "./tree.nex"))
 system(paste("rm", "./data.txt"))
+try(system(paste("rm", "./data.txt.PP.trees")))
+try(system(paste("rm", "./data.txt.PP.txt")))
 
 # RETURN RESULTS
 return(Results)
