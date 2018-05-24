484d260561d44266d43b298645f41ac0f23400d5
Randi Griffin <rgriff23@users.noreply.github.com>
2018-05-24 19:17:22 -0400

    update killbt

--- R/killbt.R
+++ R/killbt.R
@@ -1,5 +1,20 @@
-killbt = function() {
-	jobs = system("pgrep BayesTraits", intern=T)
-	for (n in 1:length(jobs)) {system(paste("kill", jobs[n]))}
+# FUNCTION TO KILL ALL BAYESTRAITS PROCESSES
+killbt <- function() {
+  
+  # DETECT SYSTEM
+  if (.Platform$OS.type == "windows") {
+    windows = TRUE
+  } else if (Sys.info()["sysname"] == "Darwin") {
+    windows = FALSE
+  } else {stop("Operating system not supported")}
+  
+  # KILL PROCESSES
+  if (windows) {
+    system('taskkill /f /im BayesTraits*')
+  } else {
+    jobs <- system("pgrep BayesTraits", intern=T)
+    for (n in 1:length(jobs)) {system(paste("kill", jobs[n]))}
+  }
+  
 }
 
--- R/lrtest.R
+++ R/lrtest.R
@@ -1,20 +1,19 @@
-lrtest = function (model1, model2) {
+lrtest <- function (model1, model2) {
   model1 <- model1$Log$results
   model2 <- model2$Log$results
 	if (nrow(model1) != nrow(model2)) {stop("Objects must contain the same number of models.")}
-	Lhs = c(mean(model1$Lh), mean(model2$Lh))
-	ind = sort(Lhs, index.return=TRUE)$ix
-	max = which.max(c(model1$Lh, model2$Lh))
-	Lh1 = Lhs[ind[1]]
-	Lh2 = Lhs[ind[2]]
-	LRstat = c()
-	pval = c()
+	Lhs <- c(mean(model1$Lh), mean(model2$Lh))
+	ind <- sort(Lhs, index.return=TRUE)$ix
+	max <- which.max(c(model1$Lh, model2$Lh))
+	Lh1 <- Lhs[ind[1]]
+	Lh2 <- Lhs[ind[2]]
+	LRstat <- pval <- c()
 	for (n in 1:length(Lh1)) {
-		lrs = 2*(Lh1[n] - Lh2[n])
-		if (Lh1[n] < Lh2[n]) {lrs = -lrs}
-		pv = pchisq(lrs, df=1, lower.tail=F) 
-		LRstat = c(LRstat, lrs)
-		pval = c(pval, pv)
+		lrs <- 2*(Lh1[n] - Lh2[n])
+		if (Lh1[n] < Lh2[n]) {lrs <- -lrs}
+		pv <- pchisq(lrs, df=1, lower.tail<-F) 
+		LRstat <- c(LRstat, lrs)
+		pval <- c(pval, pv)
 	}
 return(data.frame(model1$Lh, model2$Lh, LRstat, pval))
 }
